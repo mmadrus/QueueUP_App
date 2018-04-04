e13281893836cc8bb3862a67dfa6dcad4d35dd9b
Liridon Smajli <vondony@hotmail.com>
2018-04-04 11:24:36 +0200

    Test, Liridon.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,4 +6,5 @@ public class Controller {
     public String d = "Svej";
     public String XXX = "If you know what I mean...";
     public String johan = "Jalla re";
+    public String lirdion = "Va";
 }
