11c2ce1b44ca0df081c3fdca8fb8bf5c22575b4b
mmadrus <m.madrusan@hotmail.com>
2018-04-09 12:50:48 +0200

    QUP 1.0.1 Windows size changed

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("QueueUP");
-        primaryStage.setScene(new Scene(root));
+        primaryStage.setScene(new Scene(root, 1200, 700));
         primaryStage.show();
     }
 
