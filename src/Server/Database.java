eb7a122cd1ca6dfbaaf4c7b14569f9df691eed46
Lavitz87 <johan.granberg87@gmail.com>
2018-05-02 12:40:58 +0200

    QUP 1.1.2.2 Added more methods in db

--- src/Server/Database.java
+++ src/Server/Database.java
@@ -12,8 +12,10 @@ public class Database {
     private String url = "jdbc:mysql://den1.mysql5.gear.host/qup?user=qup&password=Sv3t8?CUfd!S";
 
     // Prepared statement to add room to the db
-    private String addRoom = "insert into qup.room(room_id, room_name, room_password, isPrivate)" +
+    private String addRoomUrl = "insert into qup.room(room_id, room_name, room_password, isPrivate)" +
             " VAlUES(?,?,?,?)";
+    // Prepared statement to check if room exist
+    private String searchForRoomUrl = "select room_name from qup.room where room_name = ?";
 
     // Prepared statement to add user into the db
     private String addUserUrl = "insert into qup.user(user_id, user_name, user_password, user_mail, isAdmin, isBlocked)" +
@@ -131,6 +133,7 @@ public class Database {
         return exist;
     }
 
+    // Method to update user textArea
     public ArrayList<String> userList(){
             ArrayList<String>userList = new ArrayList<>();
         try (PreparedStatement statement = c.prepareStatement(channelUserList)){
@@ -145,6 +148,66 @@ public class Database {
         }
 
         return userList;
+    }
+
+    // A method to add rooms to a database
+    public void addRoom(int id, String roomName, String roomPassword, Boolean isPrivate){
+        try (PreparedStatement statement = c.prepareStatement(addRoomUrl)) {
+
+            statement.setInt(1, id);
+            statement.setString(2,roomName);
+            statement.setString(3,roomPassword);
+            statement.setBoolean(4,false);
+
+            statement.execute();
+
+            System.out.println("Room added");
+
+        } catch (Exception e) {
+            System.out.println("FAIL");
+        }
+
+    }
+
+    // A method to search for room name in db
+    public boolean searchForRoom(String roomName) {
+
+        boolean exist = false;
+
+        try (PreparedStatement statement = c.prepareStatement(searchForRoomUrl)) {
+
+            statement.setString(1, roomName);
+            ResultSet resultSet = statement.executeQuery();
+
+            resultSet.next();
+
+
+            String room = resultSet.getString("room_name");
+
+            if (room.equals(roomName)) {
+
+                System.out.println(room);
+                exist = true;
+
+            } else {
+
+                exist = false;
+            }
+
+
+        } catch (Exception e) {
+
+            e.getSuppressed();
+        }
+
+
+        return exist;
+
+    }
+
+
+    public void channelList(){
+
     }
 
 }
