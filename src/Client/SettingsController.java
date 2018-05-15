947a4054fc71ddfa8b2e3f3c54683c3322366c23
Benka <filip.bengtsson@hotmail.com>
2018-05-15 08:46:43 +0200

    QUP 1.1.7.2 Updated settings

--- src/Client/SettingsController.java
+++ src/Client/SettingsController.java
@@ -8,8 +8,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
-
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
