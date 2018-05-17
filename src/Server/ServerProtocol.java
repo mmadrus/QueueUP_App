e821f83579708b87db309807e31ad18a29469189
Lavitz87 <johan.granberg87@gmail.com>
2018-05-17 12:27:16 +0200

    QUP 1.2.1.1 Fix

--- src/Server/ServerProtocol.java
+++ src/Server/ServerProtocol.java
@@ -14,7 +14,7 @@ public class ServerProtocol {
     public boolean databaseProtocol(String command, String data) {
 
         boolean succesfull = true;
-
+        String nuFunkarDet;
         // Add user command
         if (command.equals("/1")) {
 
